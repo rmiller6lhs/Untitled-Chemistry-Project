515277729b62c0bb6cac782fd94c47fd40839c13
richard-miller-du <rmiller6@dcsdk12.org>
2024-05-13 20:43:47 -0600

    Calculator Update
    1. Added molar mass calculation.
    2. Added % Composition function.

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 import json
-import pathlib
 import os
 from input_processor import InputProcessor
 
@@ -8,20 +7,36 @@ class Calculator:
         self.formula = formula
         self.__kwargs__ = calculations
         self.element_counts = InputProcessor(self.formula).element_counts
-        self.molar_mass()
+        if self.__kwargs__['molar_mass'] == True:
+            self.molar_mass()
+            
+        if self.__kwargs__['percent_comp'] == True:
+            self.percent_comp()
+        
+        
     def molar_mass(self):
         self.util_loader()
+        molecule_mass = 0
+        for element,count in self.element_counts.items():
+            element_dict = self.elements_data[element]
+            element_mass = element_dict['mass']
+            element_contribution = element_mass*count
+            molecule_mass += element_contribution
+            if self.__kwargs__['percent_comp'] == True:
+                self.element_counts[element] = [count,element_contribution]
+        self.molecule_mass = molecule_mass
         
+    def percent_comp(self):
+        
+        for element,value in self.element_counts.items():
+            #self.element_counts[element] = value.append(100*value[1]/self.molecule_mass)
+            print(value)
+        print(self.element_counts)
         
     def util_loader(self):
         
-        elements_fpath = os.path.dirname(os.path.abspath(__file__))
-        print(elements_fpath)
-        
+        elements_fpath = os.path.dirname(os.path.abspath(__file__)) + '/utils/elements.json'
         with open(elements_fpath,'r') as f:
-            elements_data = json.open(f)
+            self.elements_data = json.load(f)
             
-        print(elements_data)
-        
-
         
